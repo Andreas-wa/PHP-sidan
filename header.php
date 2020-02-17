162c44b124f1b8271081055a6fb86db7cef163bf
Andreas Wallström <user.name>
2020-02-17 09:47:48 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <h1>Välkommen till min sida!</h1>
+
+<hr>
+
+content
+
+<hr>
+
+    <a href="main.php ">Start</a> | <a href="about.php">Om oss</a>
+    <script src="js/main.js"></script>
+</body>
+</html>
